4d5179f35f0bbb911e888dcf51b73d52250096c7
Yathi <bm.thunderemperor@gmail.com>
2013-02-28 15:16:23 +0530

    Initial Commit

---
+++ CXFClient/src/com/client/PostStudentClient.java
@@ -0,0 +1,48 @@
+package com.client;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+
+public class PostStudentClient {
+
+	public static void main(String[] args) {
+		try {
+			URL url = new URL(
+					"http://localhost:8080/CXFRest/rest/getPostFee");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json");
+
+			String input = "{\"Student\":{\"name\":\"Tom\"}}";
+
+			OutputStream os = conn.getOutputStream();
+			os.write(input.getBytes());
+			os.flush();
+
+			Scanner scanner;
+			String response;
+			if (conn.getResponseCode() != 200) {
+				scanner = new Scanner(conn.getErrorStream());
+				response = "Error From Server \n\n";
+			} else {
+				scanner = new Scanner(conn.getInputStream());
+				response = "Response From Server \n\n";
+			}
+			scanner.useDelimiter("\\Z");
+			System.out.println(response + scanner.next());
+			scanner.close();
+			conn.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
